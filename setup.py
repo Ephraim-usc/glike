5ce7a689f3512cd6c6329123ac77e245721efc4e
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2021-10-11 11:13:06 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import dist, setup, Extension
+
+# bootstrap numpy; can we workaround this? 
+dist.Distribution().fetch_build_eggs(["numpy>=1.14.5"])
+
+# should be fine now
+import numpy as np
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="glike",
+    version="0.1",
+    author="Caoqi Fan",
+    author_email="caoqifan@usc.edu",
+    description="Genealogical likelihood",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ephraim-usc/glike.git",
+    packages=["glike"],
+    python_requires=">=3",
+    install_requires=[
+        "numpy>=1.14.5",
+        "pandas>=1.2.0",
+        "tskit",
+        "tqdm",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="coalescence",
+)
