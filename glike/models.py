a3d6be9ccd3e636c0370e22944c78df3ea7b61cf
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2021-10-22 08:30:05 -0700

    Update models.py

--- glike/models.py
+++ glike/models.py
@@ -1,2 +1,15 @@
 from .glike import *
 
+def threeway_admixture_lmp(t1, t2, r1, r2, n = 1e-4, m = 1e-4):
+  times = [0, t1, t2]
+  mss = [np.array([[0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0],
+                   [0, 0, 0, m, m],
+                   [0, 0, m, 0, m],
+                   [0, 0, m, m, 0]])] * 3
+  nss = [[n, n, n, n, n]] * 3
+  Ps = [np.identity(5),
+        np.matrix([[0, r1, 0, 0, 1-r1], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]),
+        np.matrix([[1, 0, 0, 0, 0], [0, 0, r2, 1-r2, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]])]
+  lmp = LMP(times, mss, nss, Ps = Ps)
+  return lmp
