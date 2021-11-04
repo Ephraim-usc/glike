40b708823da90598d0c7ea9eaf354f65eceaab19
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2021-11-04 11:55:56 -0700

    Update models.py

--- glike/models.py
+++ glike/models.py
@@ -7,6 +7,11 @@ def single_lmp(N):
   lmp = glike.LMP(times, mss, nss)
   return lmp
 
+def single_demography(N):
+  demography = msprime.Demography()
+  demography.add_population(name = "A", initial_size = N)
+  return demography
+
 
 def twoway_admixture_lmp(t, r, N_ab, N_a, N_b, m = 1e-4):
   times = [0, t]
@@ -18,6 +23,15 @@ def twoway_admixture_lmp(t, r, N_ab, N_a, N_b, m = 1e-4):
         np.matrix([[0, r, 1-r], [0, 1, 0], [0, 0, 1]])]
   lmp = LMP(times, mss, nss, Ps = Ps)
   return lmp
+
+def twoway_admixture_demography(t, r, N_ab, N_a, N_b, m = 1e-4):
+  demography = msprime.Demography()
+  demography.add_population(name = "AB", initial_size = N_ab)
+  demography.add_population(name = "A", initial_size = N_a)
+  demography.add_population(name = "B", initial_size = N_b)
+  demography.set_symmetric_migration_rate(["A", "B"], m)
+  demography.add_admixture(time=t, derived="AB", ancestral=["A", "B"], proportions = [r, 1-r])
+  return demography
 
 
 def threeway_admixture_lmp(t1, t2, r1, r2, N_abc, N_ab, N_a, N_b, N_c, m = 1e-4):
@@ -33,3 +47,18 @@ def threeway_admixture_lmp(t1, t2, r1, r2, N_abc, N_ab, N_a, N_b, N_c, m = 1e-4)
         np.matrix([[1, 0, 0, 0, 0], [0, 0, r2, 1-r2, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]])]
   lmp = LMP(times, mss, nss, Ps = Ps)
   return lmp
+
+def threeway_admixture_demography(t, r, N_ab, N_ab, N_a, N_b, N_c, m = 1e-4):
+  demography = msprime.Demography()
+  demography.add_population(name = "ABC", initial_size = N_abc)
+  demography.add_population(name = "AB", initial_size = N_ab)
+  demography.add_population(name = "A", initial_size = N_a)
+  demography.add_population(name = "B", initial_size = N_b)
+  demography.add_population(name = "C", initial_size = N_c)
+  demography.set_symmetric_migration_rate(["A", "B"], m)
+  demography.set_symmetric_migration_rate(["A", "C"], m)
+  demography.set_symmetric_migration_rate(["B", "C"], m)
+  demography.add_admixture(time=t, derived="ABC", ancestral=["AB", "C"], proportions = [r1, 1-r1])
+  demography.add_admixture(time=t, derived="AB", ancestral=["A", "B"], proportions = [r2, 1-r2])
+  return demography
+
