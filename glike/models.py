fed31c6b8c913fe942e512e46b9f079f3737db42
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2021-11-18 23:20:08 -0800

    Update models.py

--- glike/models.py
+++ glike/models.py
@@ -52,11 +52,21 @@ def twoway_admixture_demography(t, r, N_ab, N_a, N_b, m = 1e-4):
 
 def threeway_admixture_lmp(t1, t2, r1, r2, N_abc, N_ab, N_a, N_b, N_c, m = 1e-4):
   times = [0, t1, t2]
-  mss = [np.array([[0, 0, 0, 0, 0],
+  mss = [np.array([[0, 0, m, m, m],
+                   [0, 0, 0, 0, 0],
+                   [m, 0, 0, m, m],
+                   [m, 0, m, 0, m],
+                   [m, 0, m, m, 0]]),
+         np.array([[0, 0, 0, 0, 0],
+                   [0, 0, m, m, m],
+                   [0, m, 0, m, m],
+                   [0, m, m, 0, m],
+                   [0, m, m, m, 0]]),
+         np.array([[0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0],
                    [0, 0, 0, m, m],
                    [0, 0, m, 0, m],
-                   [0, 0, m, m, 0]])] * 3
+                   [0, 0, m, m, 0]])]
   nss = [[1/N_abc, 1/N_ab, 1/N_a, 1/N_b, 1/N_c]] * 3
   Ps = [np.identity(5),
         np.matrix([[0, r1, 0, 0, 1-r1], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]),
@@ -71,11 +81,29 @@ def threeway_admixture_demography(t1, t2, r1, r2, N_abc, N_ab, N_a, N_b, N_c, m 
   demography.add_population(name = "A", initial_size = N_a)
   demography.add_population(name = "B", initial_size = N_b)
   demography.add_population(name = "C", initial_size = N_c)
+  
   demography.set_symmetric_migration_rate(["A", "B"], m)
   demography.set_symmetric_migration_rate(["A", "C"], m)
   demography.set_symmetric_migration_rate(["B", "C"], m)
+  demography.set_symmetric_migration_rate(["ABC", "A"], m)
+  demography.set_symmetric_migration_rate(["ABC", "B"], m)
+  demography.set_symmetric_migration_rate(["ABC", "C"], m)
+  demography.set_symmetric_migration_rate(["AB", "A"], 0)
+  demography.set_symmetric_migration_rate(["AB", "B"], 0)
+  demography.set_symmetric_migration_rate(["AB", "C"], 0)
+  
   demography.add_admixture(time=t1, derived="ABC", ancestral=["AB", "C"], proportions = [r1, 1-r1])
+  demography.add_symmetric_migration_rate_change(time=t1, populations=["ABC","A"], rate=0)
+  demography.add_symmetric_migration_rate_change(time=t1, populations=["ABC","B"], rate=0)
+  demography.add_symmetric_migration_rate_change(time=t1, populations=["ABC","C"], rate=0)
+  demography.add_symmetric_migration_rate_change(time=t1, populations=["AB","A"], rate=m)
+  demography.add_symmetric_migration_rate_change(time=t1, populations=["AB","B"], rate=m)
+  demography.add_symmetric_migration_rate_change(time=t1, populations=["AB","C"], rate=m)
+  
   demography.add_admixture(time=t2, derived="AB", ancestral=["A", "B"], proportions = [r2, 1-r2])
+  demography.add_symmetric_migration_rate_change(time=t2, populations=["AB","A"], rate=0)
+  demography.add_symmetric_migration_rate_change(time=t2, populations=["AB","B"], rate=0)
+  demography.add_symmetric_migration_rate_change(time=t2, populations=["AB","C"], rate=0)
   return demography
 
 
