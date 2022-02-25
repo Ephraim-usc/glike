af191070c0f15440465e0f4265f6558a76e5d547
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2022-02-24 21:18:35 -0800

    Update gRvNN.py

--- glike/gRvNN.py
+++ glike/gRvNN.py
@@ -1,3 +1,9 @@
+import contextlib
+import io
+import sys
+
+
+import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -21,47 +27,83 @@ class gRNN(nn.Module):
         if len(children) == 0:
             output = self.activation(self.Q(torch.tensor([[time]])))
         if len(children) == 1:
-            print("Error: unary node " + str(node), flush = True)
+            print("Error: unary node.", flush = True)
         if len(children) == 2:
             left = children[0]
             right = children[1]
             output = self.activation(self.W(self.traverse(tree, left) + self.traverse(tree, right)) + self.Q(torch.tensor([[time]])))
         return output
     
-    def forward(self, tree):
-        output = self.traverse(tree, tree.root)
-        results = self.projection(output)
+    def forward(self, trees):
+        predictions = []
+        for tree in trees:
+          output = self.traverse(tree, tree.root)
+          prediction = self.projection(output)
+          predictions.append(prediction)
+        results = torch.cat(predictions, 0)
         return results
     
-    def getLoss(self, tree, target):
-        results = self.forward(tree)
-        loss = torch.mean(torch.abs((target - results) / target))
+    def getLoss(self, trees, target):
+        results = self.forward(trees)
+        loss = MAPE_loss(results, target)
         return results, loss
 
 
-demo = single_demography(10000)
-tree = msprime.sim_ancestry(demography = demo, samples = 10, ploidy = 1).first()
-print(tree.draw_text())
+model = gRNN(num_params = 5)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, dampening=0.0)
+
+for epoch in range(100):
+    trees = []
+    params = []
+    for i in range(1000):
+        t = random.random() * 1000
+        r = random.random()
+        N_ab = random.random() * 20000
+        N_a = random.random() * 20000
+        N_b = random.random() * 20000
+        demo = twoway_admixture_demography(t, r, N_ab, N_a, N_b)
+        tree = msprime.sim_ancestry(demography = demo, samples = {"AB":20}, ploidy = 1).first()
+        trees.append(tree.copy())
+        params.append([t, r, N_ab, N_a, N_b])
+    
+    params = torch.tensor(params)
+    results, loss = model.getLoss(trees, params)
+    optimizer.zero_grad()
+    loss.backward()
+    
+    save_stdout = sys.stdout
+    sys.stdout = io.open('trash', 'w')
+    clip_grad_norm_(model.parameters(), 5, norm_type=2.)
+    sys.stdout = save_stdout
+    
+    optimizer.step()
+    print(loss, flush = True)
+
+
 
 model = gRNN(num_params = 1)
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, dampening=0.0)
-results, loss = model.getLoss(tree, torch.tensor([[10000]]))
-optimizer.zero_grad()
-loss.backward()
-clip_grad_norm_(model.parameters(), 5, norm_type=2.)
-optimizer.step()
 
-
-
-
-for epoch in range(max_epochs):
-  print("Epoch %d" % epoch)
-  pbar = progressbar.ProgressBar(widgets=widgets, maxval=len(trn)).start()
-  for step, tree in enumerate(trn):
-     predictions, loss = model.getLoss(tree)
-     optimizer.zero_grad()
-     loss.backward()
-     clip_grad_norm(model.parameters(), 5, norm_type=2.)
-     optimizer.step()
-     pbar.update(step)
+for epoch in range(100):
+    trees = []
+    params = []
+    for i in range(1000):
+        N = random.random() * 20000
+        demo = single_demography(N)
+        tree = msprime.sim_ancestry(demography = demo, samples = {"A":20}, ploidy = 1).first()
+        trees.append(tree.copy())
+        params.append([N])
+    
+    params = torch.tensor(params)
+    results, loss = model.getLoss(trees, params)
+    optimizer.zero_grad()
+    loss.backward()
+    
+    save_stdout = sys.stdout
+    sys.stdout = io.open('trash', 'w')
+    clip_grad_norm_(model.parameters(), 5, norm_type=2.)
+    sys.stdout = save_stdout
+    
+    optimizer.step()
+    print(loss, flush = True)
  
