7e3b09e38c8cdcc3c2cf135f2012a65df2aedc11
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2022-04-02 11:04:39 -0700

    Update glike.py

--- glike/glike.py
+++ glike/glike.py
@@ -11,22 +11,46 @@ np.seterr(divide='ignore')
 
 
 class Stage: #lineage Markov Process
-  def __init__(self, in_pops, out_pops, P, ns):
+  def __init__(self, in_pops, out_pops, P = None):
     self.in_pops = in_pops
     self.out_pops = out_pops
-    self.ns = ns
-    self.P = P
-    self.Q = np.zeros([len(out_pops), len(out_pops)])
-    self.QQ = np.zeros([len(out_pops)**2, len(out_pops)**2])
     
-  def add_migration(source, dest, rate):
+    if P is None:
+      P = np.identity(len(in_pops))
+    
+    self.P = pd.DataFrame(P)
+    self.P.index = in_pops; self.P.columns = out_pops
+    
+    self.Q = pd.DataFrame(np.zeros([len(out_pops), len(out_pops)]))
+    self.Q.index = self.Q.columns = out_pops
+    
+    self.QQ = pd.DataFrame(np.zeros([len(out_pops)**2, len(out_pops)**2]))
+    self.QQ.index = self.QQ.columns = [x+y for x in out_pops for y in out_pops]
+  
+  def add_coalescent_rate(self, pops, n):
+    if type(pops) != list:
+      pops = [pops]
+    for pop in pops:
+      for pop2 in pops:
+      self.QQ.loc[pop+pop2, pop+pop2] = -n
+  
+  def add_migration_rate(self, source, dest, rate):
+    self.Q.loc[source, dest] += rate
+    self.Q.loc[source, source] -= rate
+    
+    for pop in self.out_pops:
+      self.QQ.loc[]
+  
+  def add_clone(self, pop1, pop2):
     return
   
-  def add_clone(pop1, pop2):
-    return
+  def _finalize(self):
+    pass
   
-  def _finalize():
-    
+  def print(self):
+    print(self.P)
+    print(self.Q)
+    print(self.QQ)
 
 
 class Demography:
