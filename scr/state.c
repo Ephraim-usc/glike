25417cf17545c42f24ff98830671931dbf32d2fb
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2022-10-07 13:14:14 -0700

    Update state.c

--- scr/state.c
+++ scr/state.c
@@ -8,7 +8,42 @@
 typedef double DTYPE;
 typedef unsigned long ITYPE;
 
-typedef struct list // list of pointers
+
+typedef struct list // list of DTYPE numbers
+{
+  DTYPE *data;
+  ITYPE used;
+  ITYPE size;
+} list;
+
+list *new_list()
+{
+  list *lst = (list *)malloc(sizeof(list));
+  lst->data = malloc(4 * sizeof(DTYPE));
+  lst->used = 0;
+  lst->size = 4;
+  return lst;
+}
+
+void insert_list(list *lst, DTYPE a) 
+{
+  if (lst->used == lst->size)
+  {
+    lst->size *= 2;
+    lst->data = realloc(lst->data, lst->size * sizeof(DTYPE));
+  }
+  lst->data[lst->used++] = a;
+}
+
+void free_list(list *lst)
+{
+  free(lst->data);
+  lst->data = NULL;
+  lst->used = lst->size = 0;
+}
+
+
+typedef struct plist // list of pointers
 {
   void **data;
   ITYPE used;
