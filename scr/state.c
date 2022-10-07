7fdf4ad29a09a73334c802e9eef20009dccc0fd4
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2022-10-07 12:03:53 -0700

    Update state.c

--- scr/state.c
+++ scr/state.c
@@ -11,28 +11,22 @@ typedef unsigned long ITYPE;
 
 typedef struct state
 {
-  ITYPE n; // number of lineages of the state
-  DTYPE t; // time of the state
-  ITYPE *lins; // lineages of the state
   ITYPE *values; // population labels of the lineages
   DTYPE logp;
   
   ITYPE num_anc; // number of ancestor states
-  void *links_anc; // links to ancestor states
+  struct state **links_anc; // links to ancestor states
   DTYPE *logps_anc;
   
   ITYPE num_des; // number of descendent states
-  void *links_des; // links to descendent states
+  struct state **links_des; // links to descendent states
   DTYPE *logps_des;
 } state;
 
-state* new_state(ITYPE n, DTYPE t, ITYPE *lins, ITYPE *values)
+state* new_state(ITYPE *values)
 {
   state* stt;
   stt = (state *)malloc(sizeof(state));
-  stt->n = n;
-  stt->t = t;
-  stt->lins = lins;
   stt->values = values;
   
   stt->num_anc = 0;
@@ -53,14 +47,36 @@ void print_state(state* stt)
     printf("%lu", *(values+i));
   printf("\n");
   
+  printf("ancestral links: ")
   for(i = 0; i < stt->num_anc; i++)
     printf("%lf", *(stt->logps_anc+i));
   printf("\n");
   
+  printf("descendent links: ")
   for(i = 0; i < stt->num_des; i++)
     printf("%lf", *(stt->logps_des+i));
   printf("\n");
 }
+
+typedef struct bundle
+{
+  ITYPE n; // number of lineages of the state
+  DTYPE t; // time of the state
+  ITYPE *lins; // lineages of the state
+  state **states;
+} bundle;
+
+bundle* new_bundle(ITYPE n, DTYPE t, ITYPE *lins)
+{
+  bundle* bdl;
+  bdl = (bundle *)malloc(sizeof(bundle));
+  bdl->n = n;
+  bdl->t = t;
+  bdl->lins = lins;
+  
+  return bdl;
+}
+
 
 
 
