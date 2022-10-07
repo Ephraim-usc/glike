c46cbae99772a9a0b40a0c81d2dc3842103e74c5
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2022-10-07 07:41:32 -0700

    Update lpv.c

--- scr/lpv.c
+++ scr/lpv.c
@@ -7,3 +7,51 @@
 
 #include "numpy/arrayobject.h"
 
+typedef double DTYPE;
+typedef unsigned long ITYPE;
+
+typedef struct state {
+  ITYPE n; // number of lineages of the state
+  DTYPE t; // time of the state
+  ITYPE *lins; // lineages of the state
+  DTYPE *values; // population labels of the lineages
+  
+  struct state **links_prev;
+  struct state **links_next;
+} state;
+
+matrix* new_state(ITYPE n)
+{
+  state* stt;
+  stt = (state *)malloc(sizeof(state));
+  mat->n = n;
+  mat->data = calloc(n*n,sizeof(DTYPE));
+  return mat;
+}
+
+void print_state(state* stt)
+{
+  ITYPE n = stt->n;
+  ITYPE t = stt->t;
+  
+  printf("state of %lu lineages at time %lf\n", n, t);
+  
+  ITYPE i, j;
+  for (i=0; i<n; i++)
+  {
+    for (j=0; j<n; j++)
+      printf("%lf ", mat->data[i*n + j]);
+    printf("\n");
+  }
+}
+
+
+
+int main()
+{
+  //matrix* mat = newMatrix(10);
+  //int idx[3] = {2,5,7}; 
+  //addSquare(mat, idx, 3, 9);
+  printf("%d", fib_(10));
+  return 0;
+}
