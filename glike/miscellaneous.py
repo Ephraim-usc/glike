467bd0fc011de42c08e46dad66e247eb9fbfdae5
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2022-12-04 11:20:03 -0800

    Update miscellaneous.py

--- glike/miscellaneous.py
+++ glike/miscellaneous.py
@@ -5,10 +5,14 @@ import gzip
 
 def write_relate_input(arg, name):
   haps_file = gzip.open(name + ".haps.gz", "wt")
-  current_position = 0
+  prev_position = 0
   for variant in arg.variants():
-    position = 
-    string = "1 snp" + str(variant.index + 1) + " " + str(variant.position) + " A" + " T "
+    position = math.ceil(variant.position)
+    if position == prev_position:
+      continue
+    prev_position = position
+    
+    string = "1 snp" + str(variant.index + 1) + " " + str(position) + " A" + " T "
     string = string + " ".join(map(str, variant.genotypes)) + "\n"
     haps_file.write(string)
   haps_file.close()
@@ -22,8 +26,14 @@ def write_relate_input(arg, name):
   
   map_file = open(name + ".map", "wt")
   map_file.write("pos COMBINED_rate Genetic_Map\n")
+  prev_position = 0
   for variant in arg.variants():
-    string = str(variant.position) + " " + str(1.0) + " "
+    position = math.ceil(variant.position)
+    if position == prev_position:
+      continue
+    prev_position = position
+    
+    string = str(position) + " " + str(1.0) + " "
     string = string + str(variant.position / 1000000) + "\n"
     map_file.write(string)
   map_file.close()
