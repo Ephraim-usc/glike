f17cf8b8f4d3d86f8656d9577a273ee34ae92b79
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2022-12-04 10:53:24 -0800

    Update miscellaneous.py

--- glike/miscellaneous.py
+++ glike/miscellaneous.py
@@ -1,10 +1,26 @@
 import numpy as np
 import gzip
 
-# genotypes could be an MxN matrix or generator of size N vectors
-# loci is a size M vector
-write_relate_input(genotypes, loci, names):
-  haps_file = gzip.open(name + ".haps", "wt")
-  for i, locus in enumerate(loci):
-    string = "1 snp" + str(i + 1) + " " + str(locus) + " A" + " T "
-    string = string + " ".join(map(str, next(genotypes))) + "\n"
+
+def write_relate_input(arg, name):
+  haps_file = gzip.open(name + ".haps.gz", "wt")
+  for variant in arg.variants():
+    string = "1 snp" + str(variant.index + 1) + " " + str(variant.position) + " A" + " T "
+    string = string + " ".join(map(str, variant.genotypes)) + "\n"
+    haps_file.write(string)
+  haps_file.close()
+  
+  sample_file = open(name + ".sample", "wt")
+  sample_file.write("ID_1 ID_2 missing\n0 0 0\n")
+  for sample in range(arg.num_samples):
+    string = "UNR" + str(sample + 1) + " NA" + " 0\n"
+    bytes = sample_file.write(string)
+  sample_file.close()
+  
+  map_file = open(name + ".map", "wt")
+  map_file.write("pos COMBINED_rate Genetic_Map\n")
+  for variant in arg.variants():
+    string = str(variant.position) + " " + str(1.0) + " "
+    string = string + str(variant.position / 1000000) + "\n"
+    map_file.write(string)
+  map_file.close()
