e1426e113e860e2a891cfeda8f16728fec9c956f
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2022-12-04 23:12:54 -0800

    Update simulate.py

--- manuscript/simulate.py
+++ manuscript/simulate.py
@@ -4,10 +4,30 @@ N = 1000
 l = 30000000
 
 demography = threeway_admixture_demography(20, 50, 0.4, 0.7, 2000, 10000, 3000, 20000, 15000, 5000, 5e-3, 1e-3)
-arg = msprime.sim_ancestry({"O": N}, sequence_length = l, demography = demography, ploidy = 1)
+arg = msprime.sim_ancestry({"O": N}, sequence_length = l, recombination_rate = 1e-8, demography = demography, ploidy = 1)
 arg = msprime.sim_mutations(arg, rate = 1e-8, discrete_genome = False)
-
 trees = [arg.at(pos).copy() for pos in range(0, l, 1000000)]
+
+write_relate_input(arg, "twa")
+
+
+'''
+~/bin/Relate --mode All -m 1.25e-8 -N 30000 --memory 20 --haps twa.haps --sample twa.sample --map twa.map -o twa
+~/bin/RelateFileFormats --mode ConvertToTreeSequence -i twa -o twa
+'''
+
+arg_relate = tskit.load("twa.trees")
+trees_relate = [arg_relate.at(pos).copy() for pos in range(0, l, 1000000)]
+
+
+
+
+sample_data = get_tsinfer_sample(arg)
+arg_tsinfer = tsinfer.infer(sample_data, recombination_rate = 1e-8)
+arg_tsinfer_simplified = arg_tsinfer.simplify(filter_populations = False, filter_individuals = False, filter_sites = False, keep_unary = False)
+arg_tsdate = tsdate.date(arg_tsinfer_simplified, Ne=10000, mutation_rate = 1e-8)
+trees_tsdate = [arg_tsdate.at(pos).copy() for pos in range(0, l, 1000000)]
+
 
 
 
@@ -17,4 +37,10 @@ limits = [(0,"t2"),("t1",100),(0,1),(0,1),(100,100000),(100,100000),(100,100000)
 fixed = ["m_ab", "m_cd"]
 
 searchspace = Searchspace(names, values, limits, fixed)
-estimate(trees, threeway_admixture_demo, searchspace)
+estimate(trees_relate, threeway_admixture_demo, searchspace)
+
+
+
+
+
+
