040086cc50da9b82ce593613b5ad1e88c5d6f671
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2022-12-05 21:37:48 -0800

    Update models.py

--- glike/models.py
+++ glike/models.py
@@ -13,6 +13,17 @@ def threeway_admixture_demo(t1, t2, r1, r2, N, N_a, N_b, N_c, N_d, N_e, m_ab, m_
   demo.add_phase(ContinuousPhase(1e5, 1e6, Q = np.array([[0]]), n = [1/N_e]))
   return demo
 
+def threeway_admixture_soft_demo(t1, t2, r1, r2, N, N_a, N_b, N_c, N_d, N_e, m_ab, m_cd):
+  demo = Demography()
+  demo.add_phase(ContinuousPhase(0, t1, Q = np.zeros([1,1]), n = [1/N]))
+  demo.add_phase(DiscretePhase(t1, P = np.array([[r1, 1-r1]])))
+  demo.add_phase(ContinuousPhase(t1, t2, Q = np.array([[-m_ab, m_ab], [m_ab, -m_ab]]), n = [1/N_a, 1/N_b]))
+  demo.add_phase(DiscretePhase(t2, P = np.array([[1, 0, 0], [0, r2, 1-r2]])))
+  demo.add_phase(ContinuousPhase(t2, 100, Q = np.array([[0, 0, 0], [0, -m_cd, m_cd], [0, m_cd, -m_cd]]), n = [1/N_a, 1/N_c, 1/N_d]))
+  demo.add_phase(ContinuousPhase(100, 8e4, Q = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]), n = [1/N_a, 1/N_c, 1/N_d]))
+  demo.add_phase(ContinuousPhase(8e4, 1e6, Q = np.array([[-1e-5, 1e-5, 1e-5], [1e-5, -1e-5, 1e-5], [1e-5, 1e-5, -1e-5]]), n = [1/N_a, 1/N_c, 1/N_d]))
+  return demo
+
 def threeway_admixture_demography(t1, t2, r1, r2, N, N_a, N_b, N_c, N_d, N_e, m_ab, m_cd):
   demography = msprime.Demography()
   demography.add_population(name = "O", initial_size = N)
