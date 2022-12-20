ea4143ab7263e6dc82e51085c6b8c6eda1e1588f
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2022-12-19 20:03:53 -0800

    Update models.py

--- glike/models.py
+++ glike/models.py
@@ -1,63 +1,76 @@
 import msprime
 from .glike import *
 
-def threeway_admixture_demo(t1, t2, t3, r1, r2, N, N_a, N_b, N_c, N_d, N_e, m_ab, m_cd):
-  demo = Demography()
-  demo.add_phase(ContinuousPhase(0, t1, Q = np.zeros([1,1]), n = [1/N]))
-  demo.add_phase(DiscretePhase(t1, P = np.array([[r1, 1-r1]])))
-  demo.add_phase(ContinuousPhase(t1, t2, Q = np.array([[-m_ab, m_ab], [m_ab, -m_ab]]), n = [1/N_a, 1/N_b]))
-  demo.add_phase(DiscretePhase(t2, P = np.array([[1, 0, 0], [0, r2, 1-r2]])))
-  demo.add_phase(ContinuousPhase(t2, 100, Q = np.array([[0, 0, 0], [0, -m_cd, m_cd], [0, m_cd, -m_cd]]), n = [1/N_a, 1/N_c, 1/N_d]))
-  demo.add_phase(ContinuousPhase(100, t3, Q = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]), n = [1/N_a, 1/N_c, 1/N_d]))
-  demo.add_phase(DiscretePhase(t3, P = np.array([[1], [1], [1]])))
-  demo.add_phase(ContinuousPhase(t3, 1e6, Q = np.array([[0]]), n = [1/N_e]))
+
+def ancient_european_demo(t1, t2, t3, t4, t5, t6, r1, r2, r3, N_ana, N_neo, N_whg, N_bronze, N_yam, N_ehg, N_chg, N_ne, N_wa, N_ooa):
+  demo = Demo()
+  demo.add_phase(Phase(0, [1/N_ana, 1/N_neo, 1/N_whg, 1/N_bronze, 1/N_yam, 1/N_ehg, 1/N_chg]))
+  P_bronze_admixture = np.array([
+      [1, 0, 0, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0],
+      [0, r1, 0, 1-r1, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 1]
+  ])
+  demo.add_phase(Phase(t1, [1/N_ana, 1/N_neo, 1/N_whg, 1/N_yam, 1/N_ehg, 1/N_chg], P = P_bronze_admixture))
+  P_yam_admixture = np.array([
+      [1, 0, 0, 0, 0],
+      [0, 1, 0, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 0, r2, 1-r2],
+      [0, 0, 0, 1, 0],
+      [0, 0, 0, 0, 1]
+  ])
+  demo.add_phase(Phase(t2, [1/N_ana, 1/N_neo, 1/N_whg, 1/N_ehg, 1/N_chg], P = P_yam_admixture))
+  P_neo_admixture = np.array([
+      [1, 0, 0, 0],
+      [r3, 1-r3, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 1, 0],
+      [0, 0, 0, 1]
+  ])
+  demo.add_phase(Phase(t3, [1/N_ana, 1/N_whg, 1/N_ehg, 1/N_chg], P = P_neo_admixture))
+  P_ne_split = np.array([
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 0, 1]
+  ])
+  demo.add_phase(Phase(t4, [1/N_ana, 1/N_ne, 1/N_chg], P = P_ne_split))
+  P_wa_split = np.array([
+      [1, 0],
+      [0, 1],
+      [1, 0]
+  ])
+  demo.add_phase(Phase(t5, [1/N_wa, 1/N_ne], P = P_wa_split))
+  P_ooa_split = np.array([
+      [1],
+      [1]
+  ])
+  demo.add_phase(Phase(t6, [1/N_ooa], P = P_ooa_split))
   return demo
 
-
-def threeway_admixture_demography(t1, t2, t3, r1, r2, N, N_a, N_b, N_c, N_d, N_e, m_ab, m_cd):
+def ancient_european_demography(t1, t2, t3, t4, t5, t6, r1, r2, r3, N_ana, N_neo, N_whg, N_bronze, N_yam, N_ehg, N_chg, N_ne, N_wa, N_ooa):
   demography = msprime.Demography()
-  demography.add_population(name = "O", initial_size = N)
-  demography.add_population(name = "A", initial_size = N_a)
-  demography.add_population(name = "B", initial_size = N_b)
-  demography.add_population(name = "C", initial_size = N_c)
-  demography.add_population(name = "D", initial_size = N_d)
-  demography.add_population(name = "E", initial_size = N_e)
+  demography.add_population(name = "ana", initial_size = N_ana)
+  demography.add_population(name = "neo", initial_size = N_neo)
+  demography.add_population(name = "whg", initial_size = N_whg)
+  demography.add_population(name = "bronze", initial_size = N_bronze)
+  demography.add_population(name = "yam", initial_size = N_yam)
+  demography.add_population(name = "ehg", initial_size = N_ehg)
+  demography.add_population(name = "chg", initial_size = N_chg)
+  demography.add_population(name = "ne", initial_size = N_ne)
+  demography.add_population(name = "wa", initial_size = N_wa)
+  demography.add_population(name = "ooa", initial_size = N_ooa)
   
-  demography.add_admixture(time=t1, derived="O", ancestral=["A", "B"], proportions = [r1, 1-r1])
-  demography.add_symmetric_migration_rate_change(time=t1, populations = ["A", "B"], rate = m_ab)
-  demography.add_symmetric_migration_rate_change(time=t2, populations = ["A", "B"], rate = 0)
+  demography.add_admixture(time=t1, derived="bronze", ancestral=["neo", "yam"], proportions = [r1, 1-r1])
+  demography.add_admixture(time=t2, derived="yam", ancestral=["ehg", "chg"], proportions = [r2, 1-r2])
+  demography.add_admixture(time=t3, derived="neo", ancestral=["ana", "whg"], proportions = [r3, 1-r3])
   
-  demography.add_admixture(time=t2, derived="B", ancestral=["C", "D"], proportions = [r2, 1-r2])
-  demography.add_symmetric_migration_rate_change(time=t2, populations = ["C", "D"], rate = m_cd)
-  demography.add_symmetric_migration_rate_change(time=100, populations = ["C", "D"], rate = 0)
-  
-  demography.add_population_split(time=t3, derived=["A", "C", "D"], ancestral="E")
-  
+  demography.add_population_split(time=t4, derived=["whg", "ehg"], ancestral="ne")
+  demography.add_population_split(time=t5, derived=["ana", "chg"], ancestral="wa")
+  demography.add_population_split(time=t6, derived=["ne", "wa"], ancestral="ooa")
   return demography
 
-
-def twoway_admixture_demo(t1, t2, r, N, N_a, N_b, N_c, m_ab):
-  demo = Demography()
-  demo.add_phase(ContinuousPhase(0, t1, Q = np.zeros([1,1]), n = [1/N]))
-  demo.add_phase(DiscretePhase(t1, P = np.array([[r, 1-r]])))
-  demo.add_phase(ContinuousPhase(t1, 100, Q = np.array([[-m_ab, m_ab], [m_ab, -m_ab]]), n = [1/N_a, 1/N_b]))
-  demo.add_phase(ContinuousPhase(100, t2, Q = np.array([[0, 0], [0, 0]]), n = [1/N_a, 1/N_b]))
-  demo.add_phase(DiscretePhase(t2, P = np.array([[1], [1]])))
-  demo.add_phase(ContinuousPhase(t2, math.inf, Q = np.array([[0]]), n = [1/N_c]))
-  return demo
-
-
-def twoway_admixture_demography(t1, t2, r, N, N_a, N_b, N_c, m_ab):
-  demography = msprime.Demography()
-  demography.add_population(name = "O", initial_size = N)
-  demography.add_population(name = "A", initial_size = N_a)
-  demography.add_population(name = "B", initial_size = N_b)
-  demography.add_population(name = "C", initial_size = N_c)
-  
-  demography.add_admixture(time=t1, derived="O", ancestral=["A", "B"], proportions = [r, 1-r])
-  demography.add_symmetric_migration_rate_change(time=t1, populations = ["A", "B"], rate = m_ab)
-  demography.add_symmetric_migration_rate_change(time=100, populations = ["A", "B"], rate = 0)
-  
-  demography.add_population_split(time=t2, derived=["A", "B"], ancestral="C")
-  
-  return demography
