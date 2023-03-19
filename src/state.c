f7c3e6ff69f63350e12923332bb5d0af09ef5945
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-03-19 11:19:54 -0700

    Update state.c

--- src/state.c
+++ src/state.c
@@ -26,11 +26,43 @@ static PyObject *view(PyObject *self, PyObject *args, PyObject *kwds)
   int i, j;
   for (i = 0; i < N; i++)
     for (j = 0; j < K; j++)
-      printf("%f ", logps[N*i+j]);
+      printf("%f ", logps[K*i+j]);
   printf("\n");
   
   Py_RETURN_NONE;
 }
+
+static PyObject *product_deterministic(PyObject *self, PyObject *args, PyObject *kwds)
+{
+  int N,K;
+  double *logps;
+  PyObject *logP;
+  
+  static char *kwlist[] = {"logP", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "|O", kwlist, &logP))
+    Py_RETURN_NONE;
+  
+  N = PyArray_DIM(logP, 0);
+  K = PyArray_DIM(logP, 1);
+  logps = (double *)PyArray_DATA((PyArrayObject *)logP);
+  
+  printf("N = %d, K = %d\n", N, K);
+  
+  int i, j;
+  for (i = 0; i < N; i++)
+    for (j = 0; j < K; j++)
+      printf("%d ", logps[K*i+j] > -INFINITY);
+  printf("\n");
+  
+  Py_RETURN_NONE;
+}
+
+
+
+
+
+
+
 
 static PyMethodDef StateMethods[] = {
     {"view", (PyCFunction) view, METH_VARARGS | METH_KEYWORDS, "View the logP matrix."},
