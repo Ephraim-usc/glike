f36442ee182613f15aacc36d2006effa8bc1c06d
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-03-19 14:15:56 -0700

    Update state.c

--- src/state.c
+++ src/state.c
@@ -91,7 +91,7 @@ static PyObject *product_det(PyObject *self, PyObject *args, PyObject *kwds)
   
   
   int dims[2]; dims[0] = N; dims[1] = num; 
-  PyObject *out = PyArray_SimpleNewFromData(2, dims, NPY_INT, values);
+  PyObject *out = PyArray_SimpleNew(2, dims, NPY_INT);
   
   return out;
 }
