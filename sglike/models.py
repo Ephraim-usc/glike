932a89b62359eaa98f9afbdcf2c519906ec86d17
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-05-20 20:17:49 -0700

    Update models.py

--- sglike/models.py
+++ sglike/models.py
@@ -1,12 +1,13 @@
 import msprime
 from .sglike import *
 
+
 ########## Twoway Admixture ###########
 def threeway_split_demo(t1, t2, N_a, N_b, N_c, N_d, N_e):
   demo = Demo()
-  demo.add_phase(Phase(0, [1/N_a, 1/N_b, 1/N_c], populations = ["A", "B", "C"]))
-  demo.add_phase(Phase(t1, [1/N_d, 1/N_c], P = np.array([[1, 0], [1, 0], [0, 1]])))
-  demo.add_phase(Phase(t2, [1/N_e], P = np.array([[1], [1]])))
+  demo.add_phase(Phase(0, t1, [1/N_a, 1/N_b, 1/N_c], populations = ["A", "B", "C"]))
+  demo.add_phase(Phase(t1, t2, [1/N_d, 1/N_c], P = np.array([[1, 0], [1, 0], [0, 1]])))
+  demo.add_phase(Phase(t2, math.inf, [1/N_e], P = np.array([[1], [1]])))
   return demo
 
 def threeway_split_demography(t1, t2, N_a, N_b, N_c, N_d, N_e):
@@ -23,34 +24,13 @@ def threeway_split_demography(t1, t2, N_a, N_b, N_c, N_d, N_e):
   return demography
 
 
-########## Twoway Admixture ###########
-def twoway_admixture_demo(t1, t2, r, N, N_a, N_b, N_c):
-  demo = Demo()
-  demo.add_phase(Phase(0, [1/N]))
-  demo.add_phase(Phase(t1, [1/N_a, 1/N_b], P = np.array([[r, 1-r]])))
-  demo.add_phase(Phase(t2, [1/N_c], P = np.array([[1], [1]])))
-  return demo
-
-def twoway_admixture_demography(t1, t2, r, N, N_a, N_b, N_c):
-  demography = msprime.Demography()
-  demography.add_population(name = "O", initial_size = N)
-  demography.add_population(name = "A", initial_size = N_a)
-  demography.add_population(name = "B", initial_size = N_b)
-  demography.add_population(name = "C", initial_size = N_c)
-  
-  demography.add_admixture(time=t1, derived="O", ancestral=["A", "B"], proportions = [r, 1-r])
-  demography.add_population_split(time=t2, derived=["A", "B"], ancestral="C")
-  
-  return demography
-
-
 ########## Threeway Admixture ###########
 def threeway_admixture_demo(t1, t2, t3, r1, r2, N, N_a, N_b, N_c, N_d, N_e):
   demo = Demo()
-  demo.add_phase(Phase(0, [1/N]))
-  demo.add_phase(Phase(t1, [1/N_a, 1/N_b], P = np.array([[r1, 1-r1]])))
-  demo.add_phase(Phase(t2, [1/N_a, 1/N_c, 1/N_d], P = np.array([[1, 0, 0], [0, r2, 1-r2]])))
-  demo.add_phase(Phase(t3, [1/N_e], P = np.array([[1], [1], [1]])))
+  demo.add_phase(Phase(0, t1, [1/N]))
+  demo.add_phase(Phase(t1, t2, [1/N_a, 1/N_b], P = np.array([[r1, 1-r1]])))
+  demo.add_phase(Phase(t2, t3, [1/N_a, 1/N_c, 1/N_d], P = np.array([[1, 0, 0], [0, r2, 1-r2]])))
+  demo.add_phase(Phase(t3, math.inf, [1/N_e], P = np.array([[1], [1], [1]])))
   return demo
 
 def threeway_admixture_demography(t1, t2, t3, r1, r2, N, N_a, N_b, N_c, N_d, N_e):
@@ -69,7 +49,31 @@ def threeway_admixture_demography(t1, t2, t3, r1, r2, N, N_a, N_b, N_c, N_d, N_e
   return demography
 
 
-########## American Admixture ###########
+########## Neandertal admixture (stdpopsim 3I21) ###########
+def neandertal_admixture_demo(N_yri, N_ceu, N_nea, t1, t2, t3, t4, m):
+  Q = np.array([[0, 0, 0], [0, -m, m], [0, m, -m]])
+  demo = Demo()
+  demo.add_phase(Phase(0, t1, [1/N_yri, 1/N_ceu, 1/N_nea]))
+  demo.add_phase(Phase(t1, t2, [1/N_yri, 1/N_ceu, 1/N_nea], Q = Q))
+  demo.add_phase(Phase(t2, t3, [1/N_yri, 1/N_ceu, 1/N_nea]))
+  demo.add_phase(Phase(t3, t4, [1/N_yri, 1/N_nea], P = np.array([[1, 0], [1, 0], [0, 1]])))
+  demo.add_phase(Phase(t4, math.inf, [1/N_yri], P = np.array([[1], [1]])))
+  return demo
+
+def neandertal_admixture_demography(N_yri, N_ceu, N_nea, t1, t2, t3, t4, m):
+  demography = msprime.Demography()
+  demography.add_population(name = "YRI", initial_size = N_yri)
+  demography.add_population(name = "CEU", initial_size = N_ceu)
+  demography.add_population(name = "NEA", initial_size = N_nea)
+  demography.add_symmetric_migration_rate_change(time = t1, populations = ["CEU", "NEA"], rate = m)
+  demography.add_symmetric_migration_rate_change(time = t2, populations = ["CEU", "NEA"], rate = 0)
+  demography.add_mass_migration(t3, source = "CEU", dest = "YRI", proportion = 1)
+  demography.add_mass_migration(t4, source = "NEA", dest = "YRI", proportion = 1)
+  return demography
+
+
+
+########## American Admixture (stdpopsim 4B11) ###########
 def american_admixture_demo(t1, t2, t3, t4, r1, r2, N_afr, N_eur, N_asia, N_admix, N_ooa, N_anc, gr_eur, gr_asia, gr_admix, m1, m2, m3, m4):
   Q0 = np.array([[-m1-m2, m1, m2, 0], [m1, -m1-m3, m3, 0], [m2, m3, -m2-m3, 0], [0, 0, 0, 0]])
   Q1 = np.array([[-m1-m2, m1, m2], [m1, -m1-m3, m3], [m2, m3, -m2-m3]])
@@ -114,10 +118,10 @@ def american_admixture_demography(t1, t2, t3, t4, r1, r2, N_afr, N_eur, N_asia, 
   return demography
 
 
-########## Ancient Europe ###########
+########## Ancient Europe (stdpopsim 4A21) ###########
 def ancient_europe_demo(t1, t2, t3, t4, t5, t6, r1, r2, r3, N_ana, N_neo, N_whg, N_bronze, N_yam, N_ehg, N_chg, N_ne, N_wa, N_ooa, gr):
   demo = Demo()
-  demo.add_phase(Phase(0, [1/N_ana, 1/N_neo, 1/N_whg, (1/(N_bronze*math.exp(gr*t1)) , gr), 1/N_yam, 1/N_ehg, 1/N_chg],
+  demo.add_phase(Phase(0, t1, [1/N_ana, 1/N_neo, 1/N_whg, (1/(N_bronze*math.exp(gr*t1)) , gr), 1/N_yam, 1/N_ehg, 1/N_chg],
                       populations = ["ana", "neo", "whg", "bronze", "yam", "ehg", "chg"]))
   P_bronze_admixture = np.array([
       [1, 0, 0, 0, 0, 0],
@@ -128,7 +132,7 @@ def ancient_europe_demo(t1, t2, t3, t4, t5, t6, r1, r2, r3, N_ana, N_neo, N_whg,
       [0, 0, 0, 0, 1, 0],
       [0, 0, 0, 0, 0, 1]
   ])
-  demo.add_phase(Phase(t1, [1/N_ana, 1/N_neo, 1/N_whg, 1/N_yam, 1/N_ehg, 1/N_chg], P = P_bronze_admixture,
+  demo.add_phase(Phase(t1, t2, [1/N_ana, 1/N_neo, 1/N_whg, 1/N_yam, 1/N_ehg, 1/N_chg], P = P_bronze_admixture,
                       populations = ["ana", "neo", "whg", "yam", "ehg", "chg"]))
   P_yam_admixture = np.array([
       [1, 0, 0, 0, 0],
@@ -138,7 +142,7 @@ def ancient_europe_demo(t1, t2, t3, t4, t5, t6, r1, r2, r3, N_ana, N_neo, N_whg,
       [0, 0, 0, 1, 0],
       [0, 0, 0, 0, 1]
   ])
-  demo.add_phase(Phase(t2, [1/N_ana, 1/N_neo, 1/N_whg, 1/N_ehg, 1/N_chg], P = P_yam_admixture,
+  demo.add_phase(Phase(t2, t3, [1/N_ana, 1/N_neo, 1/N_whg, 1/N_ehg, 1/N_chg], P = P_yam_admixture,
                       populations = ["ana", "neo", "whg", "ehg", "chg"]))
   P_neo_admixture = np.array([
       [1, 0, 0, 0],
@@ -147,7 +151,7 @@ def ancient_europe_demo(t1, t2, t3, t4, t5, t6, r1, r2, r3, N_ana, N_neo, N_whg,
       [0, 0, 1, 0],
       [0, 0, 0, 1]
   ])
-  demo.add_phase(Phase(t3, [1/N_ana, 1/N_whg, 1/N_ehg, 1/N_chg], P = P_neo_admixture,
+  demo.add_phase(Phase(t3, t4, [1/N_ana, 1/N_whg, 1/N_ehg, 1/N_chg], P = P_neo_admixture,
                       populations = ["ana", "whg", "ehg", "chg"]))
   P_ne_split = np.array([
       [1, 0, 0],
@@ -155,20 +159,20 @@ def ancient_europe_demo(t1, t2, t3, t4, t5, t6, r1, r2, r3, N_ana, N_neo, N_whg,
       [0, 1, 0],
       [0, 0, 1]
   ])
-  demo.add_phase(Phase(t4, [1/N_ana, 1/N_ne, 1/N_chg], P = P_ne_split,
+  demo.add_phase(Phase(t4, t5, [1/N_ana, 1/N_ne, 1/N_chg], P = P_ne_split,
                       populations = ["ana", "ne", "chg"]))
   P_wa_split = np.array([
       [1, 0],
       [0, 1],
       [1, 0]
   ])
-  demo.add_phase(Phase(t5, [1/N_wa, 1/N_ne], P = P_wa_split,
+  demo.add_phase(Phase(t5, t6, [1/N_wa, 1/N_ne], P = P_wa_split,
                       populations = ["wa", "ne"]))
   P_ooa_split = np.array([
       [1],
       [1]
   ])
-  demo.add_phase(Phase(t6, [1/N_ooa], P = P_ooa_split,
+  demo.add_phase(Phase(t6, math.inf, [1/N_ooa], P = P_ooa_split,
                       populations = ["ooa"]))
   return demo
 
@@ -198,33 +202,31 @@ def ancient_europe_demography(t1, t2, t3, t4, t5, t6, r1, r2, r3, N_ana, N_neo, 
 ########## Native Hawaiian ###########
 def nh_demo(t1, t2, t3, t4, r1, r2, r3, N_admix, N_afr, N_eur, N_asia, N_pol, N_aa, N_ooa, N_anc, gr):
   demo = Demo()
-  demo.add_phase(Phase(0, [(1/N_admix, gr)]))
+  demo.add_phase(Phase(0, t1, [(1/N_admix, gr)]))
   P_admixture = np.array([
       [r1, r2, r3, 1-r1-r2-r3]
   ])
-  demo.add_phase(Phase(t1, [1/N_afr, 1/N_eur, 1/N_asia, 1/N_pol], P = P_admixture))
+  demo.add_phase(Phase(t1, t2, [1/N_afr, 1/N_eur, 1/N_asia, 1/N_pol], P = P_admixture))
   P_pol_split = np.array([
       [1, 0, 0],
       [0, 1, 0],
       [0, 0, 1],
       [0, 0, 1]
   ])
-  demo.add_phase(Phase(t2, [1/N_afr, 1/N_eur, 1/N_aa], P = P_pol_split))
+  demo.add_phase(Phase(t2, t3, [1/N_afr, 1/N_eur, 1/N_aa], P = P_pol_split))
   P_asia_split = np.array([
       [1, 0],
       [0, 1],
       [0, 1]
   ])
-  demo.add_phase(Phase(t3, [1/N_afr, 1/N_ooa], P = P_asia_split))
+  demo.add_phase(Phase(t3, t4, [1/N_afr, 1/N_ooa], P = P_asia_split))
   P_ooa_split = np.array([
       [1],
       [1]
   ])
-  demo.add_phase(Phase(t4, [1/N_anc], P = P_ooa_split))
+  demo.add_phase(Phase(t4, math.inf, [1/N_anc], P = P_ooa_split))
   demo.phases[0].mode = "deterministic"
   return demo
-
-
 
 def nh_demography(t1, t2, t3, t4, r1, r2, r3, N_admix, N_afr, N_eur, N_asia, N_pol, N_aa, N_ooa, N_anc, gr):
   demography = msprime.Demography()
